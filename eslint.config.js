b7014f97ce8bb844e958794dc7a3a628e61959b3
gjfLeo <gjfLeo@yeah.net>
2026-01-06 20:56:18 +0800

    feat(wiki-editor): editPage JS文件lint设置

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,24 @@
 import gjfleo from "@gjfleo/eslint-config";
 
-export default gjfleo({});
+export default gjfleo(
+  {},
+  {
+    files: ["packages/wiki-editor/assets/pages/零件_*.js"],
+    rules: {
+      "style/comma-dangle": ["warn", {
+        functions: "never",
+      }],
+      "prefer-arrow-callback": "off",
+      "no-var": "off",
+      "prefer-template": "off",
+      "vars-on-top": "off",
+      "prefer-rest-params": "off",
+      "no-console": "off",
+      "object-shorthand": ["error", "never"],
+    },
+    languageOptions: {
+      ecmaVersion: 5,
+      globals: { $: "readonly", mw: "readonly" },
+    },
+  },
+);
